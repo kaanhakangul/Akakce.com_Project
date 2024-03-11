2f475a253f9106d79c060e3606399fd910a2b7fa
kaanhakangul <kaanhakangul@gmail.com>
2024-03-11 15:16:09 +0300

    US107 adding assertions.

--- src/Tests/US107.java
+++ src/Tests/US107.java
@@ -1,9 +1,11 @@
 package Tests;
 
 import Utility.BaseDriver;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class US107 extends BaseDriver {
 
@@ -20,9 +22,14 @@ public class US107 extends BaseDriver {
         WebElement hesapSil = driver.findElement(By.xpath("//a[text()='Hesabımı Sil']"));
         hesapSil.click();
         WebElement pass3 = driver.findElement(By.xpath("//input[@id='p']"));
-        pass3.sendKeys("Sdet123**");
+        pass3.sendKeys("Hakan5535.");
         WebElement hSilBtn = driver.findElement(By.xpath("//input[@id='u']"));
         hSilBtn.click();
+
+        WebElement msg=driver.findElement(By.xpath("//*[text()='Hesabın silindi.']"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[text()='Hesabın silindi.']")));
+        Assert.assertTrue("Hesabınız silinemedi.", msg.getText().contains("Hesabın silindi."));
+
 
         BekleVeKapat();
 
